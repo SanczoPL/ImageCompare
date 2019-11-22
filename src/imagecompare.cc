e986014627cf742a7597828d5e241430e9fb10b9
SanczoPL <grzegorz600425@gmail.com>
2019-11-22 13:58:21 +0100

    Change to QJsonObject

--- src/imagecompare.cc
+++ src/imagecompare.cc
@@ -1,15 +1,12 @@
 #include "../include/imagecompare.h"
-#include <QDebug>
-#include <QJsonArray>
 #include <QJsonObject>
-#include <QMap>
 #include "compare.h"
 
 constexpr auto FILTER_NAME{ "Name" };
 
-ImageCompare::ImageCompare(QJsonArray const &a_subtractorConfiguration)
+ImageCompare::ImageCompare(QJsonObject const &a_config)
 {
-  configure(a_subtractorConfiguration);
+  configure(a_config);
 }
 
 ImageCompare::~ImageCompare()
@@ -17,27 +14,24 @@ ImageCompare::~ImageCompare()
   delete m_subtractor;
 }
 
-void ImageCompare::configure(QJsonArray const &a_subtractorConfiguration)
+void ImageCompare::configure(QJsonObject const &a_config)
 {
   delete m_subtractor;
-  // qDebug() << "ImageCompare:" << a_subtractorConfiguration << endl;
-  for (auto const &SUBTRACTOR_CONFIG : a_subtractorConfiguration)
-  {
-    auto const NAME_STRING{ SUBTRACTOR_CONFIG[FILTER_NAME].toString().toStdString() };
-    auto const NAME_SID{ SID(NAME_STRING.c_str()) };
 
-    switch (NAME_SID)
-    {
-      case SID("CodeStats2014"): m_subtractor = { new Compare::CodeStats2014{ SUBTRACTOR_CONFIG.toObject() } }; break;
-      default: H_Logger->error("Unsupported filter type: {}", NAME_STRING); break;
-    }
+  auto const NAME_STRING{ a_config[FILTER_NAME].toString().toStdString() };
+  auto const NAME_SID{ SID(NAME_STRING.c_str()) };
+
+  switch (NAME_SID)
+  {
+    case SID("CodeStats2014"): m_subtractor = { new Compare::CodeStats2014{a_config} }; break;
+    default: H_Logger->error("Unsupported filter type: {}", NAME_STRING); break;
   }
+
 }
 
 struct imageErrors ImageCompare::process(const cv::Mat_<uchar> &binary, const cv::Mat_<uchar> &gt)
 {
   m_timer.start();
-
   struct imageErrors temp_imageErrors = m_subtractor->process(binary, gt);
   m_timer.stop();
   return temp_imageErrors;
